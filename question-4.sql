e569003c984d70fee9f6a5979426552b21d1b584
khaliunt <khaliunt@uw.edu>
2023-04-11 20:16:18 -0700

    hw

--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,10 @@
 -- Which sales agent made the most in sales in 2010?
+SELECT e.FirstName, e.LastName
+FROM employees e, invoices i, customers c
+WHERE i.CustomerId = c.CustomerId
+AND c.SupportRepId = e.EmployeeId
+AND e.Title = "Sales Support Agent"
+AND i.InvoiceDate LIKE "2009%"
+GROUP BY e.FirstName AND e.LastName
+ORDER BY SUM(i.Total) desc
+Limit 1;
