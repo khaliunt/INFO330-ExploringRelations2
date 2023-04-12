c7274fe3b9a4956b0fccee7f26b09cc5dc2b66ad
khaliunt <khaliunt@uw.edu>
2023-04-11 20:17:37 -0700

    hw

--- question-5.sql
+++ question-5.sql
@@ -1,1 +1,9 @@
 -- What was the most purchased track of 2013?
+SELECT t.Name as "Track Name"
+FROM tracks t, invoice_items ii, invoices i
+WHERE ii.TrackId = t.TrackId
+AND ii.InvoiceId = i.InvoiceId
+AND i.InvoiceDate LIKE "2013%"
+GROUP BY t.Name
+ORDER BY COUNT(ii.TrackId) desc
+Limit 1;
