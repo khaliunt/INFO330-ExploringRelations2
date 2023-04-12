71921bc37a2c776887c5ea88c52d20cf775646aa
khaliunt <khaliunt@uw.edu>
2023-04-11 20:15:26 -0700

    hw

--- question-2.sql
+++ question-2.sql
@@ -1,1 +1,4 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
+SELECT e.FirstName, e.LastName, c.FirstName, c.LastName, i.InvoiceId, i.InvoiceDate
+FROM customers c, employees e, invoices i
+WHERE i.CustomerId = c.CustomerId;
