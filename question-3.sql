5ce03f77cffc28d31a38bc3bbaa15854c90a816b
khaliunt <khaliunt@uw.edu>
2023-04-11 20:15:51 -0700

    hw

--- question-3.sql
+++ question-3.sql
@@ -1,1 +1,6 @@
 -- Provide a query that includes the purchased track name AND artist name with each invoice line item.
+SELECT t.Name as "Track" , art.Name as "Artist", i.InvoiceId as "Invoice Id", i.InvoiceLineId as "Invoice Line Id"
+FROM tracks t, invoice_items i, artists art, albums al
+WHERE i.TrackId = t.TrackId
+AND al.AlbumId = t.AlbumId
+AND al.ArtistId = art.ArtistId;
